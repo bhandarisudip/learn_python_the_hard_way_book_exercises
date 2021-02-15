500691b279687900c0733064ca97e8792aac33f5
Sudip Bhandari <bhandari@sbhandari.local>
2021-02-15 11:53:53 -0500

    adds exercise 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+#Exercise 11: Asking Questions
+
+print('How old are you?',)
+age = input()
+print('How tall are you?',)
+height = input()
+print('How much do you weigh?',)
+weight = input()
+
+print("So, you are {0}, years old, {1} tall and {2} heavy".format(age,height,weight))
