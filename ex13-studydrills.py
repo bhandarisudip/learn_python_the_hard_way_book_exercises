1a2b33ea0a4c7d120999282692a8f574eac088bf
Sudip Bhandari <bhandari@sbhandari.local>
2021-02-21 17:41:21 -0500

    adds exercise 13 study drills

---
+++ ex13-studydrills.py
@@ -0,0 +1,12 @@
+#ex13: study drills
+
+from sys import argv
+
+script, first, second, third = argv
+fourth = input('Enter the fourth argument here: ')
+
+print('The script is called:', script)
+print('Your first variable is:', first)
+print('Your second variable is:', second)
+print('Your third variable is:', third)
+print('Your fourth variable is:', fourth)
